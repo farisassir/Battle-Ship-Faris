de3393bcfe0cbdf44592b62535c646ce0315a8c8
Faris Ali <faris.assir@gmail.com>
2020-01-31 22:31:51 +0100

    Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        BattleShipProgram   program = new BattleShipProgram();
+        program.start();
     }
 }
