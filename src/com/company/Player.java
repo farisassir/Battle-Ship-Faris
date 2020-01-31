48a0ccc6500928ef8129a0c6c5dcb987687292ba
Faris Ali <faris.assir@gmail.com>
2020-01-31 22:23:27 +0100

    Add needed Method

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,12 +1,49 @@
 package com.company;
 
+import java.util.Random;
+
 public abstract class Player {
 
     private String name;
+    private Map map;
 
 
     public Player(String name) {
         this.name = name;
+        map = new Map();
+    }
+
+    public abstract Position getShotCoordinates();
+
+    public void placeShips() {
+        Random rand = new Random();
+        int[] shipSizes = {3, 4};
+        Position startPos;
+
+        for (int size : shipSizes) {
+
+            do {
+                int x = rand.nextInt(Map.GRID_SIZE - size + 1);
+                int y = rand.nextInt(Map.GRID_SIZE);
+                startPos = new Position(x, y);
+            } while (map.addShip(startPos, size) == false);
+
+        }
+
+    }
+    public void renderMap() {
+        System.out.println(name + " map:");
+        map.render();
+    }
+
+    public void shootAt(Position pos) {
+        map.shootAt(pos);
+
+    }
+
+    public boolean hasFloatingShips() {
+        return map.hasFloatingShips();
 
     }
 }
+
