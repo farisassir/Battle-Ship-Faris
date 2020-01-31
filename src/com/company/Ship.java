3662c8136a10a60dbc7681aa9832f6917098dbf1
Faris Ali <faris.assir@gmail.com>
2020-01-31 22:28:48 +0100

    Fix and add more methods

--- src/com/company/Ship.java
+++ src/com/company/Ship.java
@@ -1,4 +1,45 @@
 package com.company;
 
 public class Ship {
+    private Position[] positions;
+    private int size;
+    private int hits = 0;
+
+    public Ship(int size, Position startPos) {
+        this.size = size;
+        positions = getPositions(size, startPos);
+    }
+
+    public static Position[]
+    getPositions(int size, Position startPos) {
+        Position[] calculatedPositions = new Position[size];
+        calculatedPositions[0] = startPos;
+
+        for (int i = 1; i < size; i++) {
+            calculatedPositions[i] = new Position(startPos.x + i, startPos.y);
+        }
+        return calculatedPositions;
+    }
+
+    public boolean isFloating() {
+        if (hits < size)
+            return true;
+        return false;
+    }
+
+    public boolean hasPosition(Position pos) {
+        for (Position myPos : positions) {
+            if (myPos.x == pos.x && myPos.y == pos.y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addHit()
+    {
+        hits++;
+    }
 }
+
+
