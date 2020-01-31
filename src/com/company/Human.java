316146bbb8e36ec9e4ec4a0c50806c27b8c81c86
Faris Ali <faris.assir@gmail.com>
2020-01-31 22:27:59 +0100

    Fix

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,7 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Human extends Player{
     public Human(String name) {
         super("Human");
     }
+    public Human() {
+        super("Human");
+    }
+
+    public Position getShotCoordinates() {
+        System.out.println("Enter x and y coordinates, e.g. 1,3");
+        Scanner scanner = new Scanner(System.in);
+        String coords = scanner.nextLine();
+        String[] coordParts = coords.split(",");
+
+        return new Position(
+                Integer.parseInt(coordParts[0]),
+                Integer.parseInt(coordParts[1]));
+    }
+
 }
