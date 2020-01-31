ba9c9549bcfd8e61a2b53861a7d6a2195565fa13
Faris Ali <faris.assir@gmail.com>
2020-01-31 22:24:12 +0100

    Add needed Method

--- src/com/company/Computer.java
+++ src/com/company/Computer.java
@@ -1,7 +1,19 @@
 package com.company;
 
+import java.util.Random;
+
 public class Computer extends Player {
-    public Computer(String name) {
+    public Computer() {
         super("Computer");
+    }
+
+    public Position getShotCoordinates() {
+        Random rand = new Random();
+        Position position = new Position(
+                rand.nextInt(Map.GRID_SIZE),
+                rand.nextInt(Map.GRID_SIZE));
+        System.out.printf("Computer shoots at: (%d,%d) \n",
+                position.x, position.y);
+        return position;
     }
 }
